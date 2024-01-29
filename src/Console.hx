1c7c4299910647ffa0a4f157082b0d6f216de04f
One Nose <idohaolam@gmail.com>
2024-01-29 11:39:54 +0200

    Style Console.hx

--- src/Console.hx
+++ src/Console.hx
@@ -1,53 +1,59 @@
+/**
+    Singleton class for in-game debug console.
+    Press '/' to open
+**/
 class Console extends h2d.Console {
-	public static var ME : Console;
+    public static var ME: Console;
 
-	var flags : Map<String,Bool>;
+    var flags: Map<String, Bool>;
 
-	public function new() {
-		super(Assets.font);
-		h2d.Console.HIDE_LOG_TIMEOUT = 30;
-		ME = this;
-		Main.ME.root.add(this, Const.DP_UI);
-		dn.Lib.redirectTracesToH2dConsole(this);
+    public function new() {
+        super(Assets.font);
 
-		flags = new Map();
+        h2d.Console.HIDE_LOG_TIMEOUT = 30;
+        ME = this;
+        Main.ME.root.add(this, Const.DP_UI);
+        dn.Lib.redirectTracesToH2dConsole(this);
 
-		this.addCommand("fps", [], function(v:Int) set("fps",!has("fps")) );
-		#if debug
-		this.addCommand("set", [{ name:"k", t:AString }], function(k:String) {
-			set(k,true);
-			log("+ "+k, 0x80FF00);
-		});
-		this.addCommand("unset", [{ name:"k", t:AString, opt:true } ], function(?k:String) {
-			if( k==null ) {
-				log("Reset all.",0xFF0000);
-				flags = new Map();
-			}
-			else {
-				log("- "+k,0xFF8000);
-				set(k,false);
-			}
-		});
-		this.addAlias("+","set");
-		this.addAlias("-","unset");
+        this.flags = new Map();
 
-		this.addCommand("grid", [], function() {
-			var level = Game.ME.level;
-			var g = level.debug;
-			g.endFill();
-			g.lineStyle(1,0xFFFF00,0.4);
-			for(cx in 0...level.wid) {
-				g.moveTo(cx*Const.GRID,0);
-				g.lineTo(cx*Const.GRID,level.hei*Const.GRID);
-			}
-			for(cy in 0...level.hei) {
-				g.moveTo(0, cy*Const.GRID);
-				g.lineTo(level.wid*Const.GRID, cy*Const.GRID);
-			}
-		});
-		#end
-	}
+        #if debug
+        this.addCommand("set", [{name: "flag", t: AString}], function(flag: String) {
+            this.set(flag, true);
+            this.log('+ $flag', 0x80FF00);
+        });
+        this.addCommand("unset", [{name: "flag", t: AString, opt: true}], function(?flag: String) {
+            if (flag == null) {
+                this.log("Reset all.", 0xFF0000);
+                this.flags = new Map();
+            } else {
+                this.log('- $flag', 0xFF8000);
+                this.set(flag, false);
+            }
+        });
+        this.addAlias("+", "set");
+        this.addAlias("-", "unset");
 
-	public function set(k:String,v) return flags.set(k,v);
-	public function has(k:String) return flags.get(k)==true;
+        this.addCommand("grid", [], function() {
+            final level = Game.ME.level;
+            var graphics = level.debug;
+            graphics.endFill();
+            graphics.lineStyle(1, 0xFFFF00, 0.4);
+            for (cx in 0...level.wid) {
+                graphics.moveTo(cx * Const.GRID, 0);
+                graphics.lineTo(cx * Const.GRID, level.hei * Const.GRID);
+            }
+            for (cy in 0...level.hei) {
+                graphics.moveTo(0, cy * Const.GRID);
+                graphics.lineTo(level.wid * Const.GRID, cy * Const.GRID);
+            }
+        });
+        #end
+    }
+
+    public function set(flag: String, value: Bool)
+        return this.flags.set(flag, value);
+
+    public function has(flag: String)
+        return this.flags.get(flag) == true;
 }
