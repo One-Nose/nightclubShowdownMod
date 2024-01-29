d61936438402466781a49bd59d5c9cbf723b933f
One Nose <idohaolam@gmail.com>
2024-01-29 11:59:24 +0200

    Style Const.hx

--- src/Const.hx
+++ src/Const.hx
@@ -1,19 +1,19 @@
 class Const {
-	public static var FPS = 60;
-	public static var GRID = 16;
-	public static var SCALE = 5;
+    public static var FPS = 60;
+    public static var GRID = 16;
+    public static var SCALE = 5;
 
-	public static var UNIQ = 0;
-	public static var INFINITE = 999999;
-	public static var PAUSE_SLOWMO= 0.03;
+    public static var UNIQ = 0;
+    public static var INFINITE = 999999;
+    public static var PAUSE_SLOWMO = 0.03;
 
-	static var _inc=0;
-	public static var DP_BG = _inc++;
-	public static var DP_FX_BG = _inc++;
-	public static var DP_PROPS = _inc++;
-	public static var DP_MOBS = _inc++;
-	public static var DP_HERO = _inc++;
-	public static var DP_FX_TOP = _inc++;
-	public static var DP_UI = _inc++;
-	public static var DP_TOP = _inc++;
+    static var index = 0;
+    public static final DP_BG = index++;
+    public static final DP_FX_BG = index++;
+    public static final DP_PROPS = index++;
+    public static final DP_MOBS = index++;
+    public static final DP_HERO = index++;
+    public static final DP_FX_TOP = index++;
+    public static final DP_UI = index++;
+    public static final DP_TOP = index++;
 }
