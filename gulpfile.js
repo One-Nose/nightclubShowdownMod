bb1aa21f036ba23b0b240d980d21a6a40a67a7f3
temp98498 <temp98498@gmail.com>
2024-09-08 15:29:09 +0300

    chore: add automatic texture packer

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+import { dest, src } from "gulp";
+import texturePacker from "gulp-free-tex-packer";
+
+export default () =>
+  src("art/export/*", { encoding: false })
+    .pipe(
+      texturePacker({
+        textureName: "gameElements",
+        removeFileExtension: true,
+        packer: "OptimalPacker",
+        exporter: "Spine",
+      })
+    )
+    .pipe(dest("res/"));
